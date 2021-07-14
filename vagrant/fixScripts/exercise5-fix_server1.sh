cd9bd9cd830e8b7dc67bfa700ec47aa3b3225a5b
Tom <tom_avronim@bitbucket.org>
2021-07-14 12:26:11 +0300

    my solutions

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+echo 'Host server2
+    User vagrant
+    StrictHostKeyChecking no
+    IdentityFile /home/vagrant/.ssh/id_rsa.pub' >> /home/vagrant/.ssh/config
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+hostname=`hostname`
+export SERVER_DES=server1
+if [[ $hostname == *"server1"* ]]; then
+  export SERVER_DES=server2
+fi
+declare -a args=("$@")
+dest_path=${args[$#-1]}
+total=0
+for ((i=0;i<=$#-2;i++));do
+echo $dest_path
+scp -i ~/.ssh/id_rsa.pub ${args[$i]} vagrant@$SERVER_DES:$dest_path
+echo $(stat -c "%s" ${args[$i]})
+total=$(($(stat -c "%s" ${args[$i]}) + $total))
+done
+echo $total
